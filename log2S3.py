7de848509bbb04bb3fa9e5cc232f75e8d065f2c0
Herve <dockerlite@gmail.com>
2017-10-18 13:30:55 +0000

    added cronfile

--- log2S3.py
+++ log2S3.py
@@ -14,6 +14,7 @@ s3 = boto3.resource('s3')
 # create a web log bucket
 s3_client = boto3.client('s3')
 
+#-------- web log files ----------------------
 web_file = os.listdir(web_log)
 weblog_bucket_name = datetime.datetime.now().strftime('weblog_%d_%m_%Y_%H_%M')
 
@@ -25,3 +26,16 @@ for log_file in web_file:
     ft= web_log + log_file
     data = open(ft, 'rb')
     s3.Bucket(weblog_bucket_name).put_object(Key=log_file ,Body=data)
+
+#------- mysql log file ------------------------
+mysql_file = os.listdir(mysql_log)
+mysql_bucket_name = datetime.datetime.now().strftime('mysqllog_%d_%m_%Y_%H_%M')
+
+s3_client.create_bucket(Bucket=mysql_bucket_name)
+
+for log_file in mysql_file:
+    # write a bucket
+    ft= mysql_log + log_file
+    data = open(ft, 'rb')
+    s3.Bucket(mysql_bucket_name).put_object(Key=log_file ,Body=data)
+
