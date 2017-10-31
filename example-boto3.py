61a6e0da34887da42311160f77c6858554dd3e9e
Herve Meftah <hmeftah@yahoo.com>
2017-10-31 14:11:14 +0100

    changed for example of how to delete bucket

--- example-boto3.py
+++ example-boto3.py
@@ -2,19 +2,33 @@
 
 import boto3
 
-# get S3 resource
+# get S3
+# resource
 s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
 
 # list des buckets
 for bucket in s3.buckets.all():
    print bucket.name
+   bucket = s3.Bucket(bucket.name)
+   # suggested by Jordon Philips
+   #bucket.objects.all().delete()
+   # for obj in bucket.objects.all():
+   #    obj.delete()
+   #bucket.delete()
+
+
+
+
+   #s3_client.delete_object(Bucket=bucket.name, Key='test-db.sql')
+
 
 # create a bucket 
-s3_client = boto3.client('s3')
-s3_client.create_bucket(Bucket='51.255.211.214-mybucket')
+#s3_client = boto3.client('s3')
+#s3_client.create_bucket(Bucket='51.255.211.214-mybucket')
 
 # write a bucket 
-data = open('test.jpg', 'rb')
-s3.Bucket('51.255.211.214-mybucket').put_object(Key="test.jpg",Body=data)
+#data = open('test.jpg', 'rb')
+#s3.Bucket('51.255.211.214-mybucket').put_object(Key="test.jpg",Body=data)
 
 
