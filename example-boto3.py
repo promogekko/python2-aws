e6e123846c9eb74d93b9d23bb2de2061fa5b6d53
Herve <dockerlite@gmail.com>
2017-11-22 14:38:02 +0000

    remove S3 bucket list

--- example-boto3.py
+++ example-boto3.py
@@ -7,15 +7,6 @@ import boto3
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
-# list des buckets
-for bucket in s3.buckets.all():
-   print bucket.name
-   #bucket = s3.Bucket(bucket.name)
-   # suggested by Jordon Philips
-   #bucket.objects.all().delete()
-   #for obj in bucket.objects.all():
-   #    obj.delete()
-
 
 # create a bucket 
 s3_client = boto3.client('s3')
